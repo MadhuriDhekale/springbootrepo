cec0309bb585a75861a475ce6fbe03e570ef326e
MadhuriDhekale <madhuridhekale1905@gmail.com>
2022-08-29 17:29:40 +0530

    initial commit

---
+++ SpringBootCore/SpringBootCore/src/main/java/com/csi/controller/ApplicationController.java
@@ -0,0 +1,27 @@
+package com.csi.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class ApplicationController {
+
+    @GetMapping
+    public  String sayHello(){
+
+        return  "Welcome Hii";
+}
+    @GetMapping("/services")
+    public  String sayServices(){
+        return  "Maths Development";
+    }
+
+    @GetMapping("/hi")
+    public  String say(){
+
+        return  "Welcome ";
+    }
+
+}
